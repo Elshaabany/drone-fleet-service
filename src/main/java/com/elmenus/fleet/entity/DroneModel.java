23059542eb76cc8b5cbc06757e96292f13ca9ba0
Elshaabany <elshaabany@protonmail.com>
2023-11-20 18:52:11 +0200

    Add: DroneModel entity class

---
+++ src/main/java/com/elmenus/fleet/entity/DroneModel.java
@@ -0,0 +1,60 @@
+package com.elmenus.fleet.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Drone_Model")
+public class DroneModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "model", nullable = false, length = 50)
+    private String model;
+
+    @Column(name = "max_weight", nullable = false, columnDefinition = "DECIMAL(5,2)")
+    private Double maxWeight;
+
+    public DroneModel() {
+
+    }
+
+    public DroneModel(String model, Double maxWeight) {
+        this.model = model;
+        this.maxWeight = maxWeight;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Double getMaxWeight() {
+        return maxWeight;
+    }
+
+    public void setMaxWeight(Double maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    @Override
+    public String toString() {
+        return "DroneModel{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", maxWeight=" + maxWeight +
+                '}';
+    }
+}
