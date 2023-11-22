4854f4d9340546ee52e183fed4d5e3db9b2f2a5e
Elshaabany <elshaabany@protonmail.com>
2023-11-22 22:35:51 +0200

    Add exception handling

---
+++ src/main/java/com/elmenus/fleet/rest/DroneExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.elmenus.fleet.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class DroneExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<DroneErrorResponse> handleException(Exception exc) {
+
+        DroneErrorResponse error = new DroneErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
