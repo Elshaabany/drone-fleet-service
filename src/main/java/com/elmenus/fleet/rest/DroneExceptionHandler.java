d9353854aceb15b55e557476139698ea9146afb4
Elshaabany <elshaabany@protonmail.com>
2023-11-23 16:13:58 +0200

    Add handler for not found exception

--- src/main/java/com/elmenus/fleet/rest/DroneExceptionHandler.java
+++ src/main/java/com/elmenus/fleet/rest/DroneExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.elmenus.fleet.rest;
 
+import com.elmenus.fleet.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,6 +8,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class DroneExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<DroneErrorResponse> handleNotFoundException(NotFoundException exc) {
+
+        DroneErrorResponse error = new DroneErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
 
     @ExceptionHandler
     public ResponseEntity<DroneErrorResponse> handleException(Exception exc) {
